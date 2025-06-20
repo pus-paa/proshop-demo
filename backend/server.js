0639dc30c2705b9748207594019f7350b4743ab4
pus-paa <pussubh@gmail.com>
2025-06-20 22:43:01 +0545

    project started and base setup

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+ import connectDB from  './config/db.js';
+ import productRoutes from './routes/productRoutes.js';
+ import { notFound,errorHandler } from './middleware/errorMiddleware.js';
+ import dotenv from  'dotenv'; 
+dotenv.config();
+
+
+const port =process.env.PORT || 5000;
+
+connectDB(); //connect to MongoDB
+
+
+
+const app = express();
+ app.get('/', (req,res)=>{
+    res.send('API is running...');
+ });
+
+ app.use('/api/products', productRoutes);
+
+
+ app.use(notFound);
+ app.use(errorHandler);
+
+ 
+
+
+ app.listen(port,()=> console.log(`server running on port ${port}`))
