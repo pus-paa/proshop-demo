ba7e3321838c46ce743131c156046254ce9cca5a
pus-paa <pussubh@gmail.com>
2025-06-22 21:34:50 +0545

    authorization added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 //@desc Auth user & get the token
 //@route POST/api/users/login
@@ -10,6 +11,18 @@ const authUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    //set JWT as HTTP-Only cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    });
+
     res.json({
       _id: user.id,
       name: user.name,
@@ -36,7 +49,11 @@ const registerUser = asyncHandler(async (req, res) => {
 //@access Private
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 //@desc Get user profile
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import userRoutes from "./routes/userRoutes.js";
 
@@ -15,6 +16,9 @@ const app = express();
 //Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Cookie parser midddleware
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
