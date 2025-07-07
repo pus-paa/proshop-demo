5afd901b4798b8cceb8733510017c8620dace5e4
pus-paa <pussubh@gmail.com>
2025-07-08 00:44:43 +0545

    Create Get Ordres

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,38 @@ import Order from "../models/orderModel.js";
 //@access private
 
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send("add order items");
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        products: x._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
 });
 
 //@desc Get logged in user orders
@@ -14,5 +45,53 @@ const addOrderItems = asyncHandler(async (req, res) => {
 //@access private
 
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.send("get my orders");
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
+
+//@desc get order by ID
+//@route GET/api/orders/:id
+//@access Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not Found");
+  }
+});
+
+//@desc Update order to paid
+//@route GET/api/orders/:id/pay
+//@access private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  res.send("update order to paid");
+});
+
+//@desc Update order to delivered
+//@route GET/api/orders/:id/deliver
+//@access private
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  res.send("update order to delivered");
+});
+
+//@desc Get all orders
+//@route GET/api/orders
+//@access Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  res.send("get all orders");
+});
+
+export {
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getOrders,
+};
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 const port = process.env.PORT || 5000;
 
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
