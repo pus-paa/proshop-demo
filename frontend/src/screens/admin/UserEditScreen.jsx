985da8461a5de6450545263846b4ab3a2940acd4
pus-paa <pussubh@gmail.com>
2025-07-15 22:39:46 +0545

    review added

--- frontend/src/screens/admin/UserEditScreen.jsx
+++ frontend/src/screens/admin/UserEditScreen.jsx
@@ -38,62 +38,69 @@ const UserEditScreen = () => {
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log("submit");
-
-    return (
-      <>
-        <Link to="/admin/userlist" className="btn btn-light my-3">
-          Go Back
-        </Link>
-        <FormContainer>
-          <h1>Edit User</h1>
-          {loadingUpdate && <Loader />}
-
-          {isLoading ? (
-            <Loader />
-          ) : error ? (
-            <Message variant="danger">
-              {error?.data?.message || error?.error || "Something went wrong"}
-            </Message>
-          ) : (
-            <Form onSubmit={submitHandler}>
-              <Form.Group controlId="name" className="my-2">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="name"
-                  placeholder="Enter name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                ></Form.Control>
-              </Form.Group>
-
-              <Form.Group controlId="email" className="my-2">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                ></Form.Control>
-              </Form.Group>
-
-              <Form.Group controlId="isAdmin" className="my-2">
-                <Form.Check
-                  type="checkbox"
-                  label="Is Admin"
-                  checked={isAdmin}
-                  onChange={(e) => setIsAdmin(e.target.checked)}
-                ></Form.Check>
-              </Form.Group>
-
-              <Button type="submit" variant="primary" className="my-2">
-                Update
-              </Button>
-            </Form>
-          )}
-        </FormContainer>
-      </>
-    );
+    try {
+      await updateUser({ userId, name, email, isAdmin });
+      toast.success("User updated successfully");
+      refetch();
+      navigate("/admin/userlist");
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
   };
+
+  return (
+    <>
+      <Link to="/admin/userlist" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit User</h1>
+        {loadingUpdate && <Loader />}
+
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">
+            {error?.data?.message || error?.error || "Something went wrong"}
+          </Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="name" className="my-2">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Enter name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="email" className="my-2">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="isAdmin" className="my-2">
+              <Form.Check
+                type="checkbox"
+                label="Is Admin"
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+
+            <Button type="submit" variant="primary" className="my-2">
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  );
 };
 export default UserEditScreen;
